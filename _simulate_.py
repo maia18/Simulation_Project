a1b95940228eb4d25aa487d5336344e57a4d0275
maia18 <nelsonkaue05100@gmail.com>
2024-03-06 23:08:30 -0300

    Update _simulate_.py

--- _simulate_.py
+++ _simulate_.py
@@ -173,25 +173,25 @@ class Simulation: # Simulation
 
     assert isinstance(ues, list) and isinstance(aps, list) # ues and aps must be an list of the PointAcess
 
-    num_ues = len(ues)
-    ap = np.random.choice(aps)
+    for ap in aps:
 
-    for i in range(num_ues):
+      for i in range(len(ues)):
+        
+        while True:
+
+          x = np.random.randint(0, 1000) 
+          y = np.random.randint(0, 1000)
+          distances_ue_ap_min = [sqrt((((x - ap.position_ap[0]) ** 2)) + (((y - ap.position_ap[1]) ** 2)))]
+          distance_ue_ap = sqrt((((x - ap.position_ap[0]) ** 2)) + (((y - ap.position_ap[1]) ** 2)))
+
+          if (self.coords.__contains__((x, y)) == False) and (distance_ue_ap >= self.do) and distance_ue_ap == min(distances_ue_ap_min):
+            
+              pos_ue = (x, y)
+              ues[i].position_ue = pos_ue
+              self.coords.append(ues[i].position_ue)
+              break
+              
       
-      while True:
-
-        x = np.random.randint(0, 1000) 
-        y = np.random.randint(0, 1000)
-        distance_ue_ap = sqrt( ( ( ( x - ap.position_ap[0] ) ** 2 ) ) + ( ( ( y - ap.position_ap[1] ) ** 2 ) ) )
-
-        if (self.coords.__contains__((x, y)) == False) and (distance_ue_ap >= self.do):
-
-          pos_ue = (x, y)
-          ues[i].position_ue = pos_ue
-          self.coords.append(ues[i].position_ue)
-          break
-
-
 
 if __name__ == "__main__":
 
@@ -219,66 +219,67 @@ if __name__ == "__main__":
   noise_power = ( ( simulate.ko ) * ( simulate.bt / len( ap.channel ) ) if len( ap.channel ) >= 0 else None ) # Noise power
 
   for _ in range(num_sms):
-    
+
+    print("- "*80) ; print(f"Simulation {_+1}") ; print("- "*80)
     simulate.UE_position(ues_, aps_)
 
     for i, ue in enumerate(ues_):
-      print(f"Position UE{i+1}: {ue.position_ue}") # Position UE
-      print(f"UE{i+1} Channel: {ue.get_channel()}") # Channel UE
+        
+        print(f"Position UE{i+1}: {ue.position_ue}") # Position UE
+        print(f"UE{i+1} Channel: {ue.get_channel()}") # Channel UE
 
-      for j, ap in enumerate(aps_):
-        distances_ue_ap_min = [sqrt((ue.position_ue[0] - ap.position_ap[0]) ** 2 + (ue.position_ue[1] - ap.position_ap[1]) ** 2) for ap in aps_]
-        distance_ue_ap = sqrt((ue.position_ue[0] - ap.position_ap[0]) ** 2 + (ue.position_ue[1] - ap.position_ap[1]) ** 2)
+        for j, ap in enumerate(aps_):
 
-        if distance_ue_ap == min(distances_ue_ap_min):
+          distances_ue_ap_min = [sqrt((ue.position_ue[0] - ap.position_ap[0]) ** 2 + (ue.position_ue[1] - ap.position_ap[1]) ** 2) for ap in aps_]
+          distance_ue_ap = sqrt((ue.position_ue[0] - ap.position_ap[0]) ** 2 + (ue.position_ue[1] - ap.position_ap[1]) ** 2)
 
-          print(f"Distance UE{i+1}-AP{j+1}: {distance_ue_ap}m") # Distance AP-UE
-          power = ( ue.power * ( simulate.k / ( distance_ue_ap ** ( simulate.n ) ) ) ) # Power in Watts
-          print(f"Power UE{i+1}-AP{j+1}: {power}W")
+          if distance_ue_ap == min(distances_ue_ap_min):
 
-        interference_ = 0
+            power = ( ue.power * ( simulate.k / ( distance_ue_ap ** ( simulate.n ) ) ) ) # Power in Watts
+            print(f"Distance UE{i+1}-AP{j+1}: {distance_ue_ap}m, Power: {power}W") # Distance AP-UE
 
-        for k_, others_ues in enumerate(ues_):
+            interference_ = 0
 
-          if ( ( others_ues.get_channel() == ue.get_channel() ) and ( others_ues != ue ) ):
+            for k_, others_ues in enumerate(ues_):
 
-            distance_othersUes_ap = sqrt( ( ( ( others_ues.position_ue[0] - ap.position_ap[0] ) ** (2) ) + ( ( others_ues.position_ue[1] - ap.position_ap[1] ) ** (2) ) ) )
-            interference_ += ((( others_ues.power * ( simulate.k / ( distance_othersUes_ap ** ( simulate.n ) ) ) ))) # interference totally
+              if ( ( others_ues.get_channel() == ue.get_channel() ) and ( others_ues != ue ) ):
 
-        if interference_ > 0:
+                distance_othersUes_ap = sqrt( ( ( ( others_ues.position_ue[0] - ap.position_ap[0] ) ** (2) ) + ( ( others_ues.position_ue[1] - ap.position_ap[1] ) ** (2) ) ) )
+                interference_ += ((( others_ues.power * ( simulate.k / ( distance_othersUes_ap ** ( simulate.n ) ) ) ))) # interference totally
 
-          sir = ((power / interference_)) # SIR in Watts
-          sinr = ((power / (interference_ + noise_power))) # SINR in Watts
-          capacity = ((simulate.bt / len(ap.channel)) * (log2(1 + sinr))) # Capacity in bps
-          
-          print("- "*80) ; print(f"Signal-to-interference ratio(SIR): {sir}W") ; sirs_totallys.append(sir)
-          print(f"Signal-to-interference-Noise ratio(SINR): {sinr}W") ; sinrs_totallys.append(sinr)
-          print(f"Capacity: {capacity}bps") ; capacities_totallys.append(capacity) ; print("- "*80) ; print("\n")
-    
-          sir_db = [10 * log10(sir_) for sir_ in sirs_totallys]
-          sinr_db = [10 * log10(sinr_) for sinr_ in sinrs_totallys]
-          capacity_db = [(capacitie_) for capacitie_ in capacities_totallys]
+            if interference_ > 0:
+
+              sir = ((power / interference_)) ; sirs_totallys.append(sir) # SIR in Watts 
+              sinr = ((power / (interference_ + noise_power))) ; sinrs_totallys.append(sinr) # SINR in Watts
+              capacity = ((simulate.bt / len(ap.channel)) * (log2(1 + sinr))) ; capacities_totallys.append(capacity) # Capacity in bps
+              
+              print("- "*80) ; print(f"SIR: {sir}W, SINR: {sinr}W, Capacity: {capacity}bps") ; print("- "*80) ; print('\n')
+
+              sir_db = [10 * log10(sir_) for sir_ in sirs_totallys]
+              sinr_db = [10 * log10(sinr_) for sinr_ in sinrs_totallys]
+              capacity_db = [(capacitie_) for capacitie_ in capacities_totallys]
 
   fig, axs = plt.subplots(2, 2, figsize=(12, 10)) # Graphic
 
   for ap in system.aps:
-    points_ap = [(ap.position_ap[0],ap.position_ap[1] + 20), (ap.position_ap[0] - 20, ap.position_ap[1] - 20), (ap.position_ap[0] + 20, ap.position_ap[1] - 20)]
-    triangle = Polygon(points_ap, closed=True, edgecolor='red', facecolor='red')
-    axs[0, 0].add_patch(triangle)
-    axs[0, 0].set_title("Simulate")
-
     for ue in system.ues:
+      points_ap = [(ap.position_ap[0],ap.position_ap[1] + 20), (ap.position_ap[0] - 20, ap.position_ap[1] - 20), (ap.position_ap[0] + 20, ap.position_ap[1] - 20)]
+      triangle = Polygon(points_ap, closed=True, edgecolor='red', facecolor='red')
       axs[0, 0].scatter(ue.position_ue[0], ue.position_ue[1], color='black', marker='.')
+      axs[0, 0].add_patch(triangle)
       axs[0, 0].set_xlim(0, 1000)
       axs[0, 0].set_ylim(0, 1000)
-      # axs[0, 0].plot([ue.position_ue[0], ap.position_ap[0]], [ue.position_ue[1], ap.position_ap[1]], linestyle='-', color='gray')
+      axs[0, 0].set_title("Simulate")
+      distances_ue_ap_min = [sqrt((ue.position_ue[0] - ap.position_ap[0]) ** 2 + (ue.position_ue[1] - ap.position_ap[1]) ** 2) for ap in aps_]
+      distance_ue_ap = sqrt((ue.position_ue[0] - ap.position_ap[0]) ** 2 + (ue.position_ue[1] - ap.position_ap[1]) ** 2)
+      if distance_ue_ap == min(distances_ue_ap_min):
+          axs[0, 0].plot([ue.position_ue[0], ap.position_ap[0]], [ue.position_ue[1], ap.position_ap[1]], linestyle='dashed', color=None)
 
+  for result, label, row, col in zip([sir_db, sinr_db, capacity_db], ['SIR', 'SINR', 'Capacity'], [0, 1, 1], [1, 0, 1]):
+    results = [value for value in result] ; results.sort()
+    cdf = np.linspace(0, 1, len(results))
+    axs[row, col].plot(results, cdf, label=f"CDF - {label}")
+    axs[row, col].set_title(f"CDF - {label}")
+    axs[row, col].grid(True)
 
-    for result, label, row, col in zip([sir_db, sinr_db, capacity_db], ['SIR', 'SINR', 'Capacity'], [0, 1, 1], [1, 0, 1]):
-      results = [value for value in result] ; results.sort()
-      cdf = np.linspace(0, 1, len(results))
-      axs[row, col].plot(results, cdf, label=f"CDF - {label}")
-      axs[row, col].set_title(f"CDF - {label}")
-      axs[row, col].grid(True)
-
-  plt.show()
+plt.show()
